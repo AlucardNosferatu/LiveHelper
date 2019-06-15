9e85d7cb5e1294215933b5cc991f346c6cac878f
Ebenezer Scrooge <1641367382@qq.com>
2019-06-15 15:51:56 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/livehelper/MainActivity.java
+++ app/src/main/java/com/example/livehelper/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+// TODO: 2019/6/15 test GithubAPI and SensorManager 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
