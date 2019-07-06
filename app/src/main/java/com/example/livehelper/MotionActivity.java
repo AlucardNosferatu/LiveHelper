8fb6fb0f049a4902c6ecd64866978a713972118f
Ebenezer Scrooge <1641367382@qq.com>
2019-07-06 19:29:04 +0800

    Update MotionActivity.java

--- app/src/main/java/com/example/livehelper/MotionActivity.java
+++ app/src/main/java/com/example/livehelper/MotionActivity.java
@@ -1,6 +1,7 @@
 package com.example.livehelper;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
@@ -12,7 +13,11 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.view.LineChartView;
 
 public class MotionActivity extends AppCompatActivity implements SensorEventListener {
@@ -20,19 +25,26 @@ public class MotionActivity extends AppCompatActivity implements SensorEventList
     private TextView ACC;
     private SensorManager SM;
     private float[] gravity = new float[3];
-    private ArrayList<Float> dataX;
-    private ArrayList<Float> dataY;
-    private ArrayList<Float> dataZ;
+    private List<PointValue> dataX;
+    private List<PointValue> dataY;
+    private List<PointValue> dataZ;
     LineChartView lineChart;
+    LineChartData LCD;
+    List<Line> Lines;
+    int counts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motion);
         isRecord=false;
-        dataX=new ArrayList<Float>();
-        dataY=new ArrayList<Float>();
-        dataZ=new ArrayList<Float>();
+        dataX=new ArrayList<PointValue>();
+        dataY=new ArrayList<PointValue>();
+        dataZ=new ArrayList<PointValue>();
         lineChart=(LineChartView)findViewById(R.id.line_chart);
+        LCD=new LineChartData();
+        Lines=new ArrayList<Line>();
+        LCD.setLines(Lines);
+        counts=0;
         ACC = (TextView) findViewById(R.id.ACC);
         SM =(SensorManager)getSystemService(Context.SENSOR_SERVICE);
     }
@@ -62,13 +74,37 @@ public class MotionActivity extends AppCompatActivity implements SensorEventList
                 gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
                 gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
                 gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-                String ACC_VALUE = "x:"
-                        + (event.values[0] - gravity[0]) + "\t" + "y:"
-                        + (event.values[1] - gravity[1]) + "\t" + "z:"
-                        + (event.values[2] - gravity[2]);
+                float VX = event.values[0] - gravity[0];
+                float VY = event.values[1] - gravity[1];
+                float VZ = event.values[2] - gravity[2];
+                String ACC_VALUE
+                        = "x:" + VX + "\t"
+                        + "y:" + VY + "\t"
+                        + "z:" + VZ;
                 if(isRecord){
-                    dataX.add()
-
+                    if(counts<10){
+                        counts++;
+                    }
+                    else{
+                        Lines.clear();
+                        counts=0;
+                        dataX.add(new PointValue(VX,dataX.size()));
+                        dataY.add(new PointValue(VY,dataY.size()));
+                        dataZ.add(new PointValue(VZ,dataZ.size()));
+                        Line LX = new Line(dataX);
+                        Line LY = new Line(dataY);
+                        Line LZ = new Line(dataZ);
+                        LX.setColor(Color.parseColor("#0000FF"));
+                        LY.setColor(Color.parseColor("#00FF00"));
+                        LZ.setColor(Color.parseColor("#FF0000"));
+                        Lines.add(LX);
+                        Lines.add(LY);
+                        Lines.add(LZ);
+                        lineChart.setLineChartData(LCD);
+                        if(dataX.size()>=20){
+                            isRecord=false;
+                        }
+                    }
                 }
                 ACC.setText(ACC_VALUE);
 
