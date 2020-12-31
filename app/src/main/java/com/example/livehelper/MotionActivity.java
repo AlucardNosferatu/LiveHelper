285cec643abfe776ed269ee582ca52106a55d00a
Ebenezer Scrooge <1641367382@qq.com>
2021-01-01 00:20:40 +0800

    Update

--- app/src/main/java/com/example/livehelper/MotionActivity.java
+++ app/src/main/java/com/example/livehelper/MotionActivity.java
@@ -25,71 +25,64 @@ public class MotionActivity extends AppCompatActivity implements SensorEventList
     Socket S=null;
     OutputStream O=null;
     BufferedWriter BW=null;
-
-    private Boolean isRecord;
+    Boolean isListening=false;
+    Boolean isSending=false;
     private TextView ACC;
     private SensorManager SM;
     private float[] gravity = new float[3];
-
-    int counts;
-    String ACC_VALUE;
-    int PortNo;
-    TextView PortNoText;
-    Thread thread;
+    private String ACC_VALUE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motion);
-        isRecord=false;
-        counts=0;
         ACC = findViewById(R.id.ACC);
         SM =(SensorManager)getSystemService(Context.SENSOR_SERVICE);
-
         //注册加速度传感器
         SM.registerListener(this,SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_UI);//采集频率
         //注册重力传感器
         SM.registerListener(this,SM.getDefaultSensor(Sensor.TYPE_GRAVITY),SensorManager.SENSOR_DELAY_FASTEST);
-
-        PortNoText=findViewById(R.id.editPort);
-        MyThread myThread = new MyThread();
-        this.thread = new Thread(myThread);
+        isListening=false;
+        isSending=false;
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         //实现接口必须重写所有方法，不想写也得留空
     }
 
-    public void MonitorSwitch(View view){
-        if(isRecord) {
-            isRecord = false;
-            try{
-                SS.close();
-                S.close();
-                O.close();
-                BW.close();
-            }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-            if(this.thread.isAlive()){
-                this.thread.interrupt();
-                this.thread = null;
-            }
+    public void SSwitch(View view){
+        if(isListening)
+        {
+
+            isListening=false;
         }
-        else{
-            if(this.thread.isAlive()){
-                this.thread.interrupt();
-                this.thread = null;
-                MyThread myThread = new MyThread();
-                this.thread = new Thread(myThread);
-                this.thread.start();
+        else
+        {
+            
+
+            isListening=true;
+        }
+    }
+
+    public void DSwitch(View view){
+        if(isListening)
+        {
+            if(isSending)
+            {
+
+                isSending=false;
             }
-            else{
-                this.thread.start();
+            else
+            {
+
+                isSending=true;
             }
-            isRecord = true;
+
+        }
+        else
+        {
+            isSending=false;
+            System.out.println("Do nothing.\n");
         }
     }
 
@@ -113,13 +106,8 @@ public class MotionActivity extends AppCompatActivity implements SensorEventList
                         + VYS + "#"
                         + VZS;
 
-                if(isRecord){
-                    if(counts<2){
-                        counts++;
-                    }
-                    else{
-                        counts=0;
-                    }
+                if(isListening)
+                {
                     ACC.setText(ACC_VALUE);
                 }
                 //重力加速度9.81m/s^2，只受到重力作用的情况下，自由下落的加速度
@@ -142,42 +130,6 @@ public class MotionActivity extends AppCompatActivity implements SensorEventList
     @Override
     protected void onPause() {
         super.onPause();
-    }
-
-
-    class MyThread implements Runnable {
-        @Override
-        public void run() {
-            FeedDicks();
-        }
-    }
-
-    private void FeedDicks(){
-        PortNo = Integer.parseInt(PortNoText.getText().toString());
-        if(PortNo < 1024 || PortNo > 65535)
-        {
-            PortNo=54500;
-        }
-        try{
-            SS = new ServerSocket(PortNo,32);
-            //等待客户端的连接
-            System.out.println("等待客户端连接！");
-            S = SS.accept();
-            System.out.println("与客户端连接成功！");
-            //调用execute()方法时，如果必要，会创建一个新的线程来处理任务，但它首先会尝试使用已有的线程，
-            //如果一个线程空闲60秒以上，则将其移除线程池；
-            //另外，任务是在Executor的内部排队，而不是在网络中排队
-            O = S.getOutputStream();
-            BW = new BufferedWriter(new OutputStreamWriter(O));
-            while(true){
-                sleep(200);
-                BW.write(ACC_VALUE);
-                BW.flush();
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
     }
 
 }
