94f67197c3c852031f8978ffdb86c7571768b395
Ebenezer Scrooge <1641367382@qq.com>
2023-06-21 12:32:23 +0800

    Update

--- BOSSCrawler/boss.py
+++ BOSSCrawler/boss.py
@@ -1,86 +1,57 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 13 22:45:48 2019
-
-@author: Scrooge
-"""
-
+import random
 import re
-import requests
-import os
-import sys
+import time
 
-path = os.path.abspath(os.path.dirname(sys.argv[0]))
+# BeautifulSoup用于处理获取的网页数据
+import pandas as pd
+# 导入正则表达式，绘图，时间，随机模块
+from bs4 import BeautifulSoup
+# 数据分析必备类库
+# 绘图模块
+from selenium import webdriver
 
+# 自动化控制浏览器模块
 
-def job_ads():
-    url = 'https://www.zhipin.com/web/geek/job?city=101230100'
-    activity = BossLocalJob()
-    activity.change_page(url, 10)
-    activity.get_source()
-    activity.get_all_events()
-    activity.get_entity()
-    activity.save_entity()
+job = "数据分析"  # 设置需要爬取的职业信息
+page = 3  # 设置爬取页数
+htmls_list = []  # 建立网页信息存储列表
+for num in range(1, page, 1):
+    url = "https://www.zhipin.com/c101010100/?query={}&page=1&ka=page-{}".format(job, num)
+    driver = webdriver.Chrome()  # 初始化webdriver
+    driver.get(url)  # 自动化运行页面
+    htmls = driver.page_source  # 获取页面信息
+    htmls_list.append(str(htmls))  # 将获取页面信息添加至网页存储列表
+    driver.close()  # 关闭浏览器
+    ran_time = random.randint(1, 5)  # 随机生成停顿时间
+    time.sleep(ran_time)  # 程序休眠
 
+info_list = []  # 建立获取职位信息存储列表
+soup = None
+for htmls in htmls_list:
+    soup = BeautifulSoup(htmls, parser="lxml")  # 解析网页
+    for i in soup.find_all("div", class_="job-primary"):
+        job = i.find_all("a")  # 获取招聘岗位信息
+        area = i.find_all('span', class_='job-area')  # 获取工作地点
+        salary = i.find_all('span', class_='red')  # 获取薪酬信息
+        title = i.find_all("h3")[1].get_text()  # 获取企业名称
+        industry = i.find_all('a', class_="false-link")[0].get_text()  # 获取所属行业
+        edu = i.find_all('p')[0].text  # 获取学历要求
+        scale = i.find_all('p')[1].text  # 获取条件信息
+        url = "https://www.zhipin.com/" + i.find_all("div", class_="primary-box")[0]['href']  # 获取详情页信息
+        # 将所有信息保存至列
+        info_list.append([title, industry, job[0]['title'], area[0].get_text(), edu, scale, salary[0].get_text(), url])
+        data = pd.DataFrame(info_list,
+                            columns=['企业名称', '所属行业', '招聘岗位', '工作地点', '条件', '企业信息', '薪资', 'URL'])
+        # 将所有内容保存为DataFrame格式。
+        data = data.drop_duplicates(subset='企业名称', keep='first')
+        # 将数据按照招聘企业去重处理
+        data['max_salary'] = data['薪资'].map(lambda x: re.findall(r"-(\d.*?)K", x)[0])
+        data['max_salary'] = data['max_salary'].astype(int) * 1000
+        # 通过正则表达式获取薪资的最大值，并整理为数值型
 
-class BossLocalJob(object):
-    def __init__(self):
-        self.entities = []
-        self.events = []
-        self.page_group = None
-        self.html_texts = []
-        print('开始爬取内容')
+        data['min_salary'] = data['薪资'].map(lambda x: re.findall(r"(\d.*?)-", x)[0])
+        data['min_salary'] = data['min_salary'].astype(int) * 1000
+        # 通过正则表达式获取薪资的最小值，并整理为数值型
 
-    # 获取单个连接html文本
-    def get_source(self):
-        self.html_texts.clear()
-        for url in self.page_group:
-            html = requests.get(url)
-            html.encoding = 'utf-8'
-            self.html_texts.append(html.text)
-
-    # 根据页数获取所有的链接
-    def change_page(self, url, total_page):
-        start_page_num = int(re.search('start=(\d+)', url, re.S).group(1))
-        page_group = []
-        for i in range(start_page_num, total_page + 1):
-            per_link = re.sub('start=\d+', 'start=%s' % (i * 10), url, re.S)
-            page_group.append(per_link)
-        self.page_group = page_group
-
-    def get_all_events(self):
-        for html_text in self.html_texts:
-            a = re.search('<ul class="events-list(.*?)<div class="paginator">', html_text, re.S)
-            if a:
-                bigger_html = a.group(1)
-                events = re.findall('(<li class="list-entry".*?</p>\s+</div>\s+</li>)', bigger_html, re.S)
-            else:
-                events = []
-            self.events.append(events)
-
-    # 获取每个活动的详细信息
-    def get_entity(self):
-        for events in self.events:
-            for event in events:
-                entity = {
-                    'title': re.search('<span itemprop="summary">(.*?)</span>', event, re.S).group(1),
-                    'time': re.search('时间：</span>\s+(.*?)\s+<time', event, re.S).group(1),
-                    'position': re.search('<li title="(.*?)">\s+<span', event, re.S).group(1),
-                    'fee': re.search('<strong>(.*?)</strong>', event, re.S).group(1)
-                }
-                self.entities.append(entity)
-
-    # 将活动信息保存到文本文件中
-    def save_entity(self):
-        f = open(os.path.join(path, '.txt'), 'a', encoding='utf-8')
-        for event in self.entities:
-            f.writelines('title:' + event['title'] + '\n')
-            f.writelines('time:' + event['time'] + '\n')
-            f.writelines('position:' + event['position'] + '\n')
-            f.writelines('fee:' + event['fee'] + '\n')
-            f.writelines('\n')
-        f.close()
-
-
-if __name__ == '__main__':
-    job_ads()
+        data.head()
+        # 看一下数据的基本情况
