9e0a8385167e4d2a4654560c6483fe00f21e720f
Ebenezer Scrooge <1641367382@qq.com>
2023-06-21 15:58:48 +0800

    Update

--- BOSSCrawler/boss.py
+++ BOSSCrawler/boss.py
@@ -1,5 +1,6 @@
 import pickle
 import random
+import threading
 import time
 
 # BeautifulSoup用于处理获取的网页数据
@@ -8,29 +9,51 @@ from bs4 import BeautifulSoup
 # 数据分析必备类库
 # 绘图模块
 from selenium import webdriver
+from tqdm import tqdm
 
 # 自动化控制浏览器模块
 
 job = '工程师'  # 设置需要爬取的职业信息
 page = 100  # 设置爬取页数
 htmls_list = []  # 建立网页信息存储列表
-driver = webdriver.Chrome()  # 初始化webdriver
-for num in range(1, page + 1, 1):
-    url = "https://www.zhipin.com/web/geek/job?city=101230100&page={}".format(num)
-    if job is not None:
-        url += '&query={}'.format(job)
-    print('Now crawling:', url)
-    driver.get(url)  # 自动化运行页面
-    time.sleep(15)
-    htmls = driver.page_source  # 获取页面信息
-    htmls_list.append(str(htmls))  # 将获取页面信息添加至网页存储列表
-    ran_time = random.randint(1, 10)  # 随机生成停顿时间
-    time.sleep(ran_time)  # 程序休眠
-driver.close()  # 关闭浏览器
+drivers_ = []
+status = []
+for i in range(8):
+    drivers_.append(webdriver.Chrome())
+    status.append(False)
+
+
+def crawl_source(worker_id, drivers):
+    for num in range(1, page + 1, 1):
+        d_index = num % len(drivers)
+        if d_index == worker_id:
+            url = "https://www.zhipin.com/web/geek/job?city=101230100&page={}".format(num)
+            if job is not None:
+                url += '&query={}'.format(job)
+            print('Now crawling:', url)
+            drivers[worker_id].get(url)  # 自动化运行页面
+            time.sleep(10)
+            page_source = drivers[d_index].page_source  # 获取页面信息
+
+            htmls_list.append(str(page_source))  # 将获取页面信息添加至网页存储列表
+            ran_time = random.randint(1, 5)  # 随机生成停顿时间
+            time.sleep(ran_time)  # 程序休眠
+    status[worker_id] = True
+
+
+threads = []
+for i in range(len(drivers_)):
+    threads.append(threading.Thread(target=crawl_source, args=(i, drivers_)))
+for i in range(len(threads)):
+    threads[i].start()
+while False in status:
+    time.sleep(1)
+for driver in drivers_:
+    driver.close()  # 关闭浏览器
 
 info_list = []  # 建立获取职位信息存储列表
 soup = None
-for htmls in htmls_list:
+for htmls in tqdm(htmls_list):
     soup = BeautifulSoup(htmls, "lxml")  # 解析网页
     job_list = soup.find_all("li", class_="job-card-wrapper")
     for i in job_list:
--- JobPlanner.py
+++ JobPlanner.py
@@ -5,13 +5,16 @@ import nltk
 
 data = pickle.load(open('BOSSCrawler/job_info.pkl', 'rb'))
 data = [jieba.lcut(' '.join(info)) for info in data]
-data = [[info_ for info_ in info if len(info_.strip()) > 1] for info in data]
+data = [[info_.lower() for info_ in info if len(info_.strip()) > 1] for info in data]
 all_word = []
 for info in data:
     all_word += info
-blacklist = ['工程师']
+blacklist = [
+    '工程师', '开发', '实施', 'cad', '专业', '经验', 'bim', '计算机相关', '设备', '电气工程师', '工艺', '算法', '技术',
+    'python', 'solidworks', '五险', 'proe', '射频', '相关', '工程', '设计'
+]
 all_word = [word for word in all_word if word not in blacklist]
 freq_dist = nltk.FreqDist(all_word)
-top10 = freq_dist.most_common(10)
-pickle.dump(top10, open('BOSSCrawler/top10.pkl', 'wb'))
+top_k = freq_dist.most_common(20)
+pickle.dump(top_k, open('BOSSCrawler/top_k.pkl', 'wb'))
 print('Done')
